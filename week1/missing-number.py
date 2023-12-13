e343853bcb17de702508afd86577e0cb8bc02655
muaz-ab <113766225+muaz-ab@users.noreply.github.com>
2023-12-13 15:23:14 +0300

    Added solution for Missing Number

--- week1/missing-number.py
+++ week1/missing-number.py
@@ -1,8 +1,4 @@
 class Solution:
-    def missingNumber(self, nums):
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        missing_number = expected_sum - actual_sum
-        return missing_number
-
+    def missingNumber(self, nums: List[int]) -> int:
+      n = len(nums)
+      return ((n * (n+1)) // 2 ) - sum(nums)
