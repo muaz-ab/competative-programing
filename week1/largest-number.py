26de9c818ee11314e7fb0b9ac24c88e2f9acc7ed
muaz-ab <113766225+muaz-ab@users.noreply.github.com>
2023-12-29 17:42:57 +0300

    Added solution for Largest Number

--- week1/largest-number.py
+++ week1/largest-number.py
@@ -1,15 +1,10 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        result = ""
-        for i in range(len(nums)-1):
-            for j in range(len(nums)-i-1):
-                if int(str(nums[j]) + str(nums[j+1])) < int(str(nums[j+1]) + str(nums[j])):
-                    nums[j] , nums[j+1] = nums[j+1] , nums[j]
-                else:
-                    continue
-        for nu in nums:
-            result += str(nu)
+        def compare(x, y):
+            return int(str(x) + str(y)) - int(str(y) + str(x))
         
+        nums.sort(key=cmp_to_key(compare), reverse=True)
+        result = ''.join(map(str, nums))   
         result = result.lstrip('0') or '0'
+
         return result
-                        
