3af2de291e07336c1b5f487c1c3bd8a10893eecc
muaz-ab <113766225+muaz-ab@users.noreply.github.com>
2024-02-19 15:00:53 +0300

    Add solution for Sort Colors

--- leetcode/sort-colors.py
+++ leetcode/sort-colors.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
         for i in range(len(nums)-1):
             for j in range(len(nums)-i-1):
                 if nums[j] > nums[j + 1]:
